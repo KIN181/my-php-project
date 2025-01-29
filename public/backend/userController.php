b273cf3ca8f2500450aed72c65ace2f1713e978e
takatoshi <kanamoritakatoshi@gmail.com>
2025-01-29 15:24:27 +0900

    create a new method of enployer

--- public/backend/userController.php
+++ public/backend/userController.php
@@ -4,18 +4,33 @@ class User {
 
     public function __construct($user) {
         $this->user = $user;
-        echo $user;
+        echo $user . "<br>";
     }
 
     public function index($add) {
-        $add = $this->user *= 2;
-        echo $add;
+        $this->user *= 78;
+        echo $this->user . "<br>";
+    }
 
+    public function enployer() {
+        $userList = [
+            "takashi" => 100,
+            "honoka" =>78,
+            "syota" =>50,
+            "ryo" =>90,
+        ];
+
+        $score = $userList;
+
+        foreach($score as $name => $points) {
+            echo $name . ":" . $points . "<br>";
+        }
     }
 }
 
 $show = new User(12345);
 $show->index(567);
+$show->enployer();
 
 
 ?>
