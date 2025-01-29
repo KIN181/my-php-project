950e4e018498ed3ae0a9c456df498a20410e390c
Takatoshi K <129253925+KIN181@users.noreply.github.com>
2025-01-29 11:34:03 +0900

    Update dbtest.php
    コメントを削除

--- public/dbtest.php
+++ public/dbtest.php
@@ -1,15 +1,15 @@
 <?php
-$servername = "mysql";  // MySQLコンテナのサービス名
-$username = "user";     // 上記で指定したMySQLユーザー
-$password = "userpassword"; // 上記で指定したMySQLパスワード
-$dbname = "mydatabase"; // 上記で指定したMySQLデータベース名
+$servername = "mysql";  
+$username = "user";     
+$password = "userpassword"; 
+$dbname = "mydatabase"; 
 
 // 接続
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // 接続チェック
 if ($conn->connect_error) {
-    die("接続失敗: " . $conn->connect_error);
+    die("失敗: " . $conn->connect_error);
 }
 echo "接続成功!";
 ?>
