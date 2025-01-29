92dab840053d5a27e72bf331716d4d185acc95cf
takatoshi <kanamoritakatoshi@gmail.com>
2025-01-29 14:48:54 +0900

    making method in userModel.php

--- public/frontend/userModel.php
+++ public/frontend/userModel.php
@@ -2,19 +2,18 @@
 class User {
     private $user;
 
-    public function construct($user) {
+    public function __construct($user) {
         $this->user = $user;
-        $userData = $user;
-        echo $userdata;
+        $user *= 40;
+        echo $user . "<br>";
     }
 
-    public function culculate() {
-        $sale = 20000;
+    public function culculate($sale) {
+        $sale *= 20;
         echo $sale;
     }
 }
 
-$culculate = new User();
-$culculate->culculate();
-
+$culculate = new User(500*2);
+$culculate->culculate(50);
 ?>
